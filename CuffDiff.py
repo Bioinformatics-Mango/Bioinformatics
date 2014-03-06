d19245692fc9dd55ddf90968f775daeb022e5941
Marcel van de Streek <marcel.streek@gmail.com>
2014-03-06 17:36:29 +0100

    Finnished first cuffdiff script

--- CuffDiff.py
+++ CuffDiff.py
@@ -17,6 +17,7 @@ import string
 import subprocess
 from subprocess import Popen, PIPE
 import os
+import sys
 
 import re
 import numpy
@@ -26,37 +27,101 @@ import numpy
 ##################################################################
 
 ## Functions
-def callCuffDiff():
+def callCuffDiff(sPath):
     ''' Calls the cuffdiff program, if not able to run the program it will give an error message
     '''
     try:    
-        if(len(subprocess.check_output(["which","cuffdiff"]))>0):
-            print('cuffdiff is installed')
-            bPermission = checkPermissions('test.txt', ['R','W','X'])
-            if(not bPermission):
-                print(bPermission)
+        if(checkRequiredFolders(sPath)): #All required files and folders are ready, creating cuffdiff commandline
+            sCuffDiffCommanLine = 'cuffdiff'
+            #Output directory
+            sCuffDiffCommanLine += ' -o ' + sPath + 'txdout/cuffdiff'
+            #Reference genome
+            sCuffDiffCommanLine += '-b' + sPath + 'ref_genome/genome.fa'
+            #Labels
+            sCuffDiffCommanLine += ' -L' + 'batch,chemostat'
+            #merged.gtf file  
+            sCuffDiffCommanLine += ' -u ' + sPath + 'txdout/merged_asm/merged.gtf'
+            #batch bam directory
+            #Create list of bam files for batch
+            sCuffDiffCommanLine += ' '
+            regTophadOut = re.compile('.*_thout')
+            for folder in os.listdir(sPath+'txdout/batch'):
+                if(len(regTophadOut.findall(folder))>=1):
+                    sCuffDiffCommanLine += sPath+'txdout/batch'+'/'+folder+'/accepted_hits.bam,'
+            #remove trailing komma
+            sCuffDiffCommanLine = sCuffDiffCommanLine[:-1]
             
-            bPermission = checkPermissions('test', ['R','W','X'])
-            if(not bPermission):
-                print(bPermission)
-            bPermission = checkPermissions('test2', ['R','W','X'])
-            if(not bPermission):
-                print(bPermission)
-            bPermission = checkPermissions('1', ['R','W','X'])
-            if(not bPermission):
-                print(bPermission)
-            bPermission = checkPermissions('1/a.txt', ['R','W','X'])
-            if(not bPermission):
-                print(bPermission)
-            bPermission = checkPermissions('3', ['R','W','X'])
-            if(not bPermission):
-                print(bPermission)
-    	else:
-    	  print('cuffdiff is not installed')
+            #Create list of bam files for chemostat
+            sCuffDiffCommanLine += ' '
+            regTophadOut = re.compile('.*_thout')
+            for folder in os.listdir(sPath+'txdout/chemostat'):
+                if(len(regTophadOut.findall(folder))>=1):
+                    sCuffDiffCommanLine += sPath+'txdout/chemostat'+'/'+folder+'/accepted_hits.bam,'
+            #remove trailing komma
+            sCuffDiffCommanLine = sCuffDiffCommanLine[:-1]
+            
+            #print(sCuffDiffCommanLine)
+            os.system(sCuffDiffCommanLine)
+            #Create list of bam files for chemostat
+            
+            
     except OSError as e:
         print(e)
     return ''
 
+def checkRequiredFolders(sPath):
+    if(len(subprocess.check_output(["which","cuffdiff"]))>0):
+        #Checking required imput files
+        sErrorMessage = ''
+        bPermission = checkPermissions(sPath + 'txdout/cuffdiff', ['R','W','X'])
+        if(not bPermission):
+            sErrorMessage += bPermission
+        bPermission = checkPermissions(sPath + 'ref_genome/genome.fa', ['R'])
+        if(not bPermission):
+            sErrorMessage += bPermission
+        bPermission = checkPermissions(sPath + 'txdout/merged_asm/merged.gtf', ['R'])
+        if(not bPermission):
+            sErrorMessage += bPermission
+            
+        #Check if the bam folders are present
+        #Batch
+        bPermission = checkPermissions(sPath + 'txdout/batch', ['R'])
+        regTophadOut = re.compile('.*_thout')
+        if(not bPermission):
+            sErrorMessage += bPermission
+        else: #check all the files in the folder
+            for folder in os.listdir(sPath+'txdout/batch'):
+                if(len(regTophadOut.findall(folder))>=1):                        #check if the accepted_hits.bam or sam file is present
+                    bPermission = checkPermissions(sPath + 'txdout/batch/' + folder + '/' + 'accepted_hits.bam', ['R'])
+                    if(not bPermission):
+                        sErrorMessage += bPermission
+                    bPermission = checkPermissions(sPath + 'txdout/batch/' + folder + '/' + 'accepted_hits.bam.bai', ['R'])
+                    if(not bPermission):
+                        sErrorMessage += bPermission
+    
+        #chemostat
+        bPermission = checkPermissions(sPath + 'txdout/chemostat', ['R'])
+                               
+        if(not bPermission):
+            sErrorMessage += bPermission
+        else: #check all the files in the folder
+            for folder in os.listdir(sPath+'txdout/chemostat'):
+                if(len(regTophadOut.findall(folder))>=1):                        #check if the accepted_hits.bam or sam file is present
+                    bPermission = checkPermissions(sPath + 'txdout/chemostat/' + folder + '/' + 'accepted_hits.bam', ['R'])
+                    if(not bPermission):
+                        sErrorMessage += bPermission
+                    bPermission = checkPermissions(sPath + 'txdout/chemostat/' + folder + '/' + 'accepted_hits.bam.bai', ['R'])
+                    if(not bPermission):
+                        sErrorMessage += bPermission                    
+        
+                    
+        
+        if(not len(sErrorMessage)==0): 
+            print(sErrorMessage)
+            sys.exit()
+    else:
+        print('cuffdiff is not installed')   
+    return True
 
 def checkPermissions(sPath, lPermissions):
     """ checkPermissions checks if this script can find the file/folder and has the correct permissions
@@ -94,7 +159,7 @@ def checkPermissions(sPath, lPermissions):
 ## Console call
 if __name__ == "__main__":
     
-    callCuffDiff()
+    callCuffDiff('/local/data/course/project/groups/mango/project/')
     
     
         
