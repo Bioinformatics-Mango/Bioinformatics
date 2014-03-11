1210a0b4f62356e236b6af0c5362f5cb1e71c0c2
nick-alberts <nick-alberts@users.noreply.github.com>
2014-03-11 22:24:55 +0100

    Update Enrichment.py

--- Project files/Enrichment.py
+++ Project files/Enrichment.py
@@ -1,47 +1,25 @@
 #!/usr/bin/env python
 """
 Name: Nick Alberts
-Function: Gene id for enrichment
+Function: retrieve ensembl_transcript_id for enrichment
 """
 import sys
-#retrieve ensmbl list from gff or gtf DONE
+#Retrieve ensmbl list from diff file extension
 ensembl_id=[]
 with open((sys.argv[1])) as infile:
   content=infile.readlines()
   infile.close
 for lines in content[1:]:
   Line=lines.split()
-  if Line[13] == 'yes':
-#if (pvalue bottom) <= Line[11]  <= (pvalue upper)
-    ensembl_id.append(Line[2])
+#Selects values between 0 and 0.05. Tab 11 contains p-values;Tab 12 contains q-values  
+  if 0 <= float(Line[11]) <= 0.05:
+        if 0 <= float(Line[12]) <= 0.05:
+          ensembl_id.append(Line[2])
   del Line
-ensembl_id
 
 #ensembl_id to new file for further processing
 with open((sys.argv[2]),'w') as outfile:
-  for ids in ensembl_id:
-    outfile.write(ids+'\n')
+  for transcript_ids in ensembl_id:
+    outfile.write(transcript_ids+'\n')
   outfile.close
 
-#input ensmbl list in david
-command=['wget http://david.abcc.ncifcrf.gov/api.jsp?type=ENSEMBL_GENE_ID&ids=','&tool=geneReportFull']
-data=[]
-
-for ids in ensembl_id[:-1]:
-  data.append(ids.replace(ids,ids+','))
-x=''.join(data)
-command.insert(1,ensembl_id[-1])
-command.insert(1,x)
-web_api=''.join(command)
-print web_api
-
-#subprocess.call(web_api)
-
-
-
-#pick DAVID options
-#wget http://david.abcc.ncifcrf.gov/api.jsp?type=ENTREZ_GENE_ID&ids=2919,6347,6348,6364&tool=geneReportFull
-#type ENSEMBL_GENE_ID
-#annot  = a list of desired annotation  categories separated by ","
-#ids  = a list of user's gene IDs separated by ","
-#tool  = one of DAVID tool names  summary
